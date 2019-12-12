3e78b52b225493b5b9bc80aa2bf334cb8ff8439d
ashea27 <shea.andrew27@gmail.com>
2019-12-12 04:15:18 -0700

    edited styles, and began rewriting game logic

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,15 @@
+const greenBtn = document.querySelector('#square-one')
+const redBtn = document.querySelector('#square-two')
+const yellowBtn = document.querySelector('#square-three')
+const blueBtn = document.querySelector('#square-four')
+
+// console.log(greenBtn, redBtn, yellowBtn, blueBtn)
+
+const gameBoard = document.querySelector('#game-board')
+
+const simonButtons = [greenBtn, redBtn, yellowBtn, blueBtn]
+// console.log(simonButtons)
+
+const gamePattern = []
+const userPattern = []
 
